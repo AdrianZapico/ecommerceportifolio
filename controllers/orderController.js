b3cb44d4932342cea50eee205af451a20c03700c
JamilEUmaNoites <adrianzsilva@outlook.com>
2025-12-28 15:37:57 -0300

    post order ok

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,48 @@
 import Order from '../models/orderModel.js';
 
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Public (should be Protected)
+const addOrderItems = async (req, res, next) => {
+    try {
+        const {
+            orderItems,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+            user // passed manually for now
+        } = req.body;
+
+        if (orderItems && orderItems.length === 0) {
+            res.status(400);
+            throw new Error('No order items');
+        } else {
+            const order = new Order({
+                orderItems,
+                user: user, // manually attached
+                shippingAddress,
+                paymentMethod,
+                itemsPrice,
+                taxPrice,
+                shippingPrice,
+                totalPrice,
+                isPaid: req.body.isPaid || false,
+                paidAt: req.body.paidAt,
+                isDelivered: req.body.isDelivered || false,
+                deliveredAt: req.body.deliveredAt,
+            });
+
+            const createdOrder = await order.save();
+            res.status(201).json(createdOrder);
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
 // @desc    Buscar todos os pedidos
 // @route   GET /api/orders
 // @access  Public
@@ -30,4 +73,4 @@ const getOrderById = async (req, res, next) => {
     }
 };
 
-export { getOrders, getOrderById };
+export { addOrderItems, getOrders, getOrderById };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import { getOrders, getOrderById } from '../controllers/orderController.js';
+import { getOrders, getOrderById, addOrderItems } from '../controllers/orderController.js';
 
-router.route('/').get(getOrders);
+router.route('/').get(getOrders).post(addOrderItems);
 router.route('/:id').get(getOrderById);
 
 export default router;
