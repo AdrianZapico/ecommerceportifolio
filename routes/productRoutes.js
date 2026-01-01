9482b6433d07d8854d6002be0fd8666b02a11b79
JamilEUmaNoites <adrianzsilva@outlook.com>
2026-01-01 14:48:38 -0300

    feat: protege rotas de admin (create/update/delete) de produtos

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,23 @@
 import express from 'express';
+import {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
 const router = express.Router();
-import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js';
 
-router.route('/').get(getProducts).post(createProduct);
-router.route('/:id').get(getProductById).put(updateProduct).delete(deleteProduct);
 
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, createProduct);
+
+
+router.route('/:id')
+    .get(getProductById)
+    .put(protect, admin, updateProduct)
+    .delete(protect, admin, deleteProduct);
 export default router;
