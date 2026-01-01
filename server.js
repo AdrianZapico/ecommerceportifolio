da6adf97f2ed0aca219a5c8af076de524bf906e2
JamilEUmaNoites <adrianzsilva@outlook.com>
2026-01-01 15:04:53 -0300

    feat: implementa rota de upload de imagens com multer

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import path from 'path'; // <--- 1. IMPORT NECESSÁRIO
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
@@ -6,6 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js'; // <--- 2. IMPORT DA ROTA
 
 dotenv.config();
 
@@ -27,6 +29,12 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes); // <--- 3. USO DA ROTA
+
+// --- CONFIGURAÇÃO DA PASTA DE IMAGENS ---
+const __dirname = path.resolve(); // Necessário porque estamos usando ES Modules
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// ----------------------------------------
 
 // Error Middleware
 app.use(notFound);
