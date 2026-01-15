c7bd3f0197378df8a3ba349bcf9ef7766f2d89e3
AdrianZapico <joeytafolla123@gmail.com>
2026-01-15 13:24:12 -0300

    cors install

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-
+import cors from 'cors';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
@@ -17,6 +17,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(cors());
 
 // Body parser middleware
 app.use(express.json());
