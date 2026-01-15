bab6b88fe7ed56c2b434e51debef7f7d200657d9
AdrianZapico <joeytafolla123@gmail.com>
2026-01-15 19:40:40 -0300

    generate token change

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,8 +1,16 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
+const generateToken = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: '30d',
+    });
+
+    // Configuração OBRIGATÓRIA para Netlify + Render
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: true, // Tem que ser true no Render (HTTPS)
+        sameSite: 'None', // <--- O SEGREDO! Permite cross-site cookie
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 dias
     });
 };
 
